6a1023c56a4b5be53121746cee6a3d3dd8a0e9d8
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2022-12-07 16:36:23 -0500

    components added

---
+++ src/resumeData.js
@@ -0,0 +1,82 @@
+let resumeData = {
+    "imagebaseurl":"profilepic.png",
+    "name": "Joseph Gossett",
+    "role": "Software Development",
+    "linkedinId":"Joseph-Gossett2022",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/joseph-gossett2022",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/blackbolt001",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"Dear Reader I have spent two decades of life working, raising a family, and just trying to give more than I get",
+    "address":"Covington,GA",
+    "website":"theForesthome.com",
+    "education":[
+      {
+        "UniversityName":"Georgia Tech",
+        "specialization":"MERN Stack",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2022",
+        "Achievements":"Full-Stack-Web-Developer"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Publix Supermarkets",
+        "specialization":"Corporate QA Food Safety Specialist, Distribution and Manufacturing Leadership, Retail - 13 years",
+        "specialistazion":" Distribution, Manufacturing, Retail, Food Safety/ Federal Regulations",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2021",
+        "Achievements":" Seafood HACCP(FDA certification)"
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Inovation"
+      },
+      {
+        "skillname":"Determination"
+      },
+      {
+        "skillname":"React "
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Website",
+        "description":"React.js web page",
+        "imgurl":"images/portfolio/profilepic.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"He is the smartest, most hansomest Man",
+        "name":"Aubree Gossett"
+      },
+      {
+        "description":"Cook's the Best Food, 5 stars",
+        "name":"My Dog"
+      },
+      { "description":"Sweetest person I know",
+      "name":"myself"
+    },
+    { 
+      "description":"I expect great things from you!",
+      "name":"My Mother"
+    },
+    { 
+      "description":"I expect great things from you!",
+      "name":"My Mother"
+    }
+    ]
+  }
+  
+  export default resumeData
